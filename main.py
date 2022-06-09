cce08c20dd00efb07fbc9e3f640bd6879c0f8079
Тимур Калинин <98173402+Remitet@users.noreply.github.com>
2022-06-09 23:38:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,60 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-import sys
-import os
 
-#Сетим логин-пароль
+
+# Сетим логин-пароль
 LOGIN = ''
 PASSWORD = ''
 
-#Раскоментировать для безоконного режима
-
+# Раскоментировать для безоконного режима
 #options = webdriver.ChromeOptions()
-#options.add_argument("--disable-blink-features=AutomationControlled")
-#options.add_argument("--headless")
+#options.headless = True
+#options.add_argument("--no-sandbox")
 
-#Путь к хромдрайверу. (для безоконного режима добавить options=options после пути к драйверу)
-driver = webdriver.Chrome(executable_path='')
+
+# Путь к хромдрайверу. (для безоконного режима добавить options=options после пути к драйверу)
+# driver = webdriver.Chrome(executable_path='')
+
 
 def Login():
+
     print("Логинимся")
     driver.get(url="https://hh.ru/account/login")
-    driver.find_element(by=By.XPATH, value="//button[@data-qa='expand-login-by-password']").click()
-    driver.find_element(by=By.XPATH, value="//input[@data-qa='login-input-username']").send_keys(LOGIN)
-    driver.find_element(by=By.XPATH, value="//input[@data-qa='login-input-password']").send_keys(PASSWORD)
-    driver.find_element(by=By.XPATH, value="//button[@data-qa='account-login-submit']").click()
+    driver.find_element(
+        by=By.XPATH, value="//button[@data-qa='expand-login-by-password']").click()
+    driver.find_element(
+        by=By.XPATH, value="//input[@data-qa='login-input-username']").send_keys(LOGIN)
+    driver.find_element(
+        by=By.XPATH, value="//input[@data-qa='login-input-password']").send_keys(PASSWORD)
+    driver.find_element(
+        by=By.XPATH, value="//button[@data-qa='account-login-submit']").click()
     time.sleep(3)
+
 
 def Bump():
-    driver.get(url="https:hh.ru/applicant/resumes?hhtmFromLabel=header&hhtmFrom=main")
-    time.sleep(3)
-    driver.find_element(by=By.XPATH, value='//button[@data-qa="resume-update-button"]').click()
-    time.sleep(2)
+    driver.get(
+        url="https:hh.ru/applicant/resumes?hhtmFromLabel=header&hhtmFrom=main")
+    time.sleep(5)
+    try:
+        driver.find_element(
+            by=By.XPATH, value='//button[@data-qa="resume-update-button"]').click()
+    except:
+        print("Не вижу кнопки, может уже нажата")
+    time.sleep(5)
     driver.quit()
-    print("На кнопку поднятия cv нажал!")
+    # print("На кнопку поднятия cv нажал!")
 
-try:
-    Login()
-    Bump()
-    print("Ждем 5 минут")
-    time.sleep(300)
-except:
-    print("Все сломалось")
 
-#Перезапускаем скрипт по кд
-python = sys.executable
-os.execl(python, python, *sys.argv)
+if __name__ == "__main__":
+    while True:
+        try:
+            driver = webdriver.Chrome(options=options)
+            Login()
+            Bump()
+            print("Кнопка успешно нажалась, ждем 4 часа...")
+            time.sleep(14401)
+
+        except:
+            print("Все сломалось, либо скрипт не нашел кнопку. \nНО ничего страшного через 4 часа скрипт перезапустится")
+            time.sleep(14401)
